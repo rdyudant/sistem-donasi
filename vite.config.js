e88660f8adbe9006b56a57f9e95cc4c41dbcdf5d
Maziz-ux <azizmuhamad018@gmail.com>
2025-07-09 16:50:37 +0700

    setting vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 export default {
   root: '.',
   server: {
-    host: '0.0.0.0',
-    port: 5173
-  },
+    host: '0.0.0.0', // biar bisa diakses dari network/domain
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      protocol: 'ws',
+      host: '36.91.103.198',
+      port: 5173
+    }
+  }
 };
